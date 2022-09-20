19feb99f7906045dc567fd7fe783f66789ea55bf
bignick1958 <bignick58@gmail.com>
2022-09-20 19:36:13 +0300

    Update 12_Calculator.py
    Размещение ячеек с полями для названий и ввода цифр

--- 12_Calculator.py
+++ 12_Calculator.py
@@ -1,9 +1,20 @@
+# My first python calculator
 import tkinter as tk
 
 window = tk.Tk()
-window.geometry(f"300x400")
+window.geometry(f"250x300")
 window['bg'] = '#8fffff'
 window.title('My Python calculator')
+
+first = tk.Label(text='Первое число')
+first.grid(column=0, row=0, padx=10, pady=10)
+second = tk.Label(text='Второе число')
+second.grid(column=0, row=1)
+
+firstEntry = tk.Entry()
+firstEntry.grid(column=1, row=0)
+secondEntry = tk.Entry()
+secondEntry.grid(column=1, row=1)
 
 
 window.mainloop()
